eeb089e6dc99a58e233f48453553f2eea2fdeabe
ymz <yahya.mzoughi@makina-corpus.com>
2013-10-21 15:38:13 +0200

    Change content mode for UIImageView, Closed #1

--- Graffitounes/FullScreenViewController.m
+++ Graffitounes/FullScreenViewController.m
@@ -26,6 +26,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    FullImage.contentMode = UIViewContentModeScaleAspectFit;
     FullImage.image = full;
 	// Do any additional setup after loading the view.
 }
